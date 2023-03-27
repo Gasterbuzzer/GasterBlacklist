935aa509e2fae8678e0af1d9e3440a7e14ca2f9c
Gasterbuzzer <juliankoberg@hotmail.com>
2023-03-27 20:29:31 +0200

    Update addURL.py

--- addURL.py
+++ addURL.py
@@ -30,8 +30,56 @@ def complex_command_handling(command):
     if command[:3] == "add":
         # Test URL: tracking.india.miui.com
         add_url(command[4:])
+    elif command[:4] == "find":
+        find_url(command[5:])
     else:
         print(f"Unknown command '{command}'. ('h' or 'help' for all commands)\n")
+
+
+def contains_http(url):
+    """Checks if string contains http returns true if true"""
+    contains = False
+
+    if "http" in url:
+        contains = True
+    elif "https" in url:
+        contains = True
+    elif "http://" in url:
+        contains = True
+    elif "https://" in url:
+        contains = True
+
+    return contains
+
+
+def find_url(url):
+    """Prints if url in list"""
+    file_name = "hosts"
+    found = False
+
+    if contains_http(url):
+        print("ERROR: url contains 'http/s' and is not valid.")
+        return
+
+    # If it includes "0.0.0.0"
+    if "0.0.0.0" in url[:8]:
+        url = url[8:]
+
+    url_to_find = "0.0.0.0 " + url + "\n"
+
+    for line in fileinput.input(files=file_name, inplace=True):
+        print(f'{line}', end='')
+
+        if line == url_to_find:
+            # Found URL
+            found = True
+
+    url_to_find = "0.0.0.0 " + url
+
+    if found:
+        print(f"\nFound URL: '{url_to_find}' in {file_name}.\n")
+    else:
+        print(f"\n{url_to_find} is not in {file_name}.\n")
 
 
 def add_url(url):
@@ -50,13 +98,21 @@ def add_url(url):
 
     # If not, then we get our current day.
     day_string = datetime.today().strftime('%d.%m.%Y')  # %Y-%m-%d
+    # day_string = "3" + day_string[1:] # Test if you want to see if the day gets added.
+
     found = False
     if day_string in text:
-        print("Day String Found")
         found = True
     else:
         # Create day comment
-        print(f"Day String not Found: {day_string}")
+        print(f"Day String not Found: {day_string}, creating one...")
+
+        for line in fileinput.input(files=file_name, inplace=True):
+            if "# END of Blacklist" in line:
+                # Adding Day String
+                print(f"\n# {day_string}\n")
+
+            print(f'{line}', end='')
 
     for line in fileinput.input(files=file_name, inplace=True):
         print(f'{line}', end='')
@@ -65,7 +121,7 @@ def add_url(url):
             # Adding URL
             print(f"0.0.0.0 {url}")
 
-
+    print(f"\nAdded URL: {url} to {file_name} at section {day_string}.\n")
 
 
 def help_print():
@@ -73,7 +129,8 @@ def help_print():
     print("\nAll commands: ")
     print("\t\t'h' or 'help' : Shows all commands (currently)")
     print("\t\t'q' or 'quit' : Quit the program")
-    print("\t\t'add'         : Adds url to list")  # (%url% should be a valid url without http or https.)
+    print("\t\t'add %url%'   : Adds url to list (%url% should be a valid url without http or https.)")
+    print("\t\t'find %url%'  : Checks if url in list (%url% should be a valid url without http or https.)")
 
     print("\n")
 
